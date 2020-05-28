75f619182fb85499619f4ceb87b9dc8ef23a9bea
Lin Du <du.lin@mail.polimi.it>
2020-05-28 10:49:19 +0200

    fix config.mk

--- config.h
+++ config.h
@@ -2,11 +2,11 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fira Code:size=12", "fontawesome:size=12" };
+static const char *fonts[]          = { "Fira Code:size=12", "fontawesome:size=14" };
 static const char dmenufont[]       = "monospace:size=12";
 // background color
 static const char col_gray1[]       = "#222222";
@@ -15,7 +15,7 @@ static const char col_gray2[]       = "#000000";
 // font color
 static const char col_gray3[]       = "#bbbbbb";
 // current tag and current window font color
-static const char col_gray4[]       = "#eaeaea";
+static const char col_gray4[]       = "#f0f0f0";
 // top bar second colour and active windows border color
 static const char col_cyan[]        = "#222222";
 static const char *colors[][3]      = {
@@ -25,7 +25,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
