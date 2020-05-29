c467c6f6969798e640fbda7aaca716e8d2557970
Lin Du <du.lin@mail.polimi.it>
2020-05-29 08:29:21 +0200

    decommented

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Fira Code:size=12", "fontawesome:size=14" };
 static const char dmenufont[]       = "monospace:size=12";
-/* default grey colours
+/* default grey colours */
 // background color
 static const char col_gray1[]       = "#222222";
 // inactive window border color
@@ -19,7 +19,7 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#f0f0f0";
 // top bar second colour and active windows border color
 static const char col_cyan[]        = "#222222";
-*/
+
 static const char col_blood_red[] = "5A1807";
 static const char col_caput_mortuum[] = "#55251D";
 static const char col_french_bistre[] = "#696047";
@@ -71,7 +71,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *roficmd[] = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *brightup[] = { "xbacklight", "-inc", "10", NULL };
