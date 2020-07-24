23034bcb43454dda22e4e46ffa7e4d2579b41574
Lin Du <du.lin@mail.polimi.it>
2020-07-24 21:30:52 +0200

    fixed offset value tdrop

--- config.h
+++ config.h
@@ -73,7 +73,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *roficmd[] = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *tdropcmd[]  = { "tdrop", "-y", "15", "-h", "400", "alacritty", "-t", "alacrittyd", NULL };
+static const char *tdropcmd[]  = { "tdrop", "-y", "20", "-h", "400", "alacritty", "-t", "alacrittyd", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
